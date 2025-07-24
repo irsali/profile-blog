ed0cc6600d61c9679b0b95945f69bea095efa2e3
Noble Wave <noble-wave@outlook.com>
2025-07-25 03:11:32 +0530

    add discussions to blog posts

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -1,8 +1,29 @@
 <script lang="ts">
 	import type { SlugPostDto } from '$lib/types';
+	import { onMount } from 'svelte';
 
 	export let data: SlugPostDto;
 	const { content, metadata } = data.post;
+
+	onMount(() => {
+		const script = document.createElement('script');
+		script.src = 'https://giscus.app/client.js';
+		script.setAttribute('data-repo', 'irsali/profile-blog');
+		script.setAttribute('data-repo-id', 'R_kgDOPSSJrg');
+		script.setAttribute('data-category', 'General');
+		script.setAttribute('data-category-id', 'DIC_kwDOPSSJrs4CtYa4');
+		script.setAttribute('data-mapping', 'title');
+		script.setAttribute('data-strict', '0');
+		script.setAttribute('data-reactions-enabled', '1');
+		script.setAttribute('data-emit-metadata', '0');
+		script.setAttribute('data-input-position', 'bottom');
+		script.setAttribute('data-theme', 'noborder_light');
+		script.setAttribute('data-lang', 'en');
+		script.setAttribute('data-loading', 'lazy');
+		script.crossOrigin = 'anonymous';
+		script.async = true;
+		document.querySelector('.giscus')?.appendChild(script);
+	});
 </script>
 
 <div class="bg-[var(--color-bg,#fff)] text-[var(--color-text,#222)]">
@@ -16,3 +37,5 @@
 		<h1 class="text-[var(--color-primary,#19c7a6)]">Not found</h1>
 	{/if}
 </div>
+
+<div class="giscus"></div>
